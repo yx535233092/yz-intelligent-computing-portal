74405351f95c1584e1cc008efc535d515c7744af
杨潇 <535233092@qq.com>
2025-08-18 09:15:24 +0800

    fix: 配图错误修复

--- src/app/industry-cases/goverment-2/page.tsx
+++ src/app/industry-cases/goverment-2/page.tsx
@@ -28,7 +28,7 @@ export default function GovernmentPolicyMatchingPage() {
     subtitle: '以企业需求为导向，实现惠企政策精准匹配与智能推送',
     description:
       '针对惠企政策知晓难、理解难、办理难、费人力、获得感不强问题，以企业需求为导向，建立统一的政策服务入口，搭建政企沟通桥梁，实现惠企政策集中汇聚、精准查询、主动推送、高效兑现。',
-    detailPicName: '某市公安情报分析以案搜案.webp',
+    detailPicName: '某市营商政策智能双向匹配.webp',
   };
 
   return (

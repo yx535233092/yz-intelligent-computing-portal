b7ba6bab1d2c520d837c32b14ec41f6f1edbd63a
杨潇 <535233092@qq.com>
2025-08-21 15:48:20 +0800

    feat: add docker-compose and nginxconf

--- python/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pprint import pprint
-import base64
-
-url = 'https://lab.iwhalecloud.com/llmdoc/v1/auth/login'
-headers ={"Content-Type":'application/json'}
-data ={"username": "h3c_yanshi","password": base64.b64encode(b'H3c@12345!')}
-response =requests.post(url,headers=headers, json=data)
-pprint(response.json())

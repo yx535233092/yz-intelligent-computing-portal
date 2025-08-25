296aa7a88537012cea6a5a98cde262e5bbfb8f8a
杨潇 <535233092@qq.com>
2025-08-25 17:36:33 +0800

    feat: 修改pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'h3c-portal',
-      script: 'node_modules/next/dist/bin/next',
+      script: 'npm',
       args: 'start',
-      cwd: './',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,

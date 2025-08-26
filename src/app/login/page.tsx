68602251caff4057b8b585da7a011b570bfe75a3
杨潇 <535233092@qq.com>
2025-08-26 12:17:05 +0800

    feat: test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,11 @@ export default function Login() {
       <canvas id="circle-big"></canvas>
       {/* 登录容器 */}
       <div className="w-[75%] h-[800px] shadow-xl  rounded-xl bg-white flex items-center justify-center">
-        <div className="w-[50%] h-full "></div>
+        <div className="w-[50%] h-full ">ces</div>
         <div className="w-[50%] h-full bg-red-400 opacity-50"></div>
       </div>
       {/* 小圆圈 */}
+
       <canvas id="circle-small"></canvas>
     </div>
   );

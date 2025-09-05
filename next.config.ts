72506826101e5b0f9034d19dc86236a3a2be1e5c
杨潇 <535233092@qq.com>
2025-09-05 17:03:11 +0800

    feat: 暂存

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   transpilePackages: ['@gradio/client'],
   // reactStrictMode: false,
+  productionBrowserSourceMaps: false, // 源码映射
 };
 
 export default nextConfig;

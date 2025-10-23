bb9d07c144926046bceebdffa591a7e1b43b2afd
杨潇 <535233092@qq.com>
2025-10-23 16:12:39 +0800

    feat: 测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: 'yz-portal-tianjing',
       script: 'npm',
       args: 'start',
-      env_file: '.env.production',
       env: {
         NODE_ENV: 'production',
         PORT: 8002,

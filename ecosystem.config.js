c965b36d4d2d51c7c2b7598537ccd35e993c89a9
杨潇 <535233092@qq.com>
2025-10-23 15:50:48 +0800

    feat: 修复环境变量加载失败

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'yz-portal-tianjing',
       script: 'npm',
       args: 'start',
+      env_file: '.env.production',
       env: {
         NODE_ENV: 'production',
         PORT: 8002,
